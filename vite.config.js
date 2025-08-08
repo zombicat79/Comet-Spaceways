36ddd8aadf5beba792d99ee7966998ef3d50fe3c
David Castejon <ombengue@hotmail.com>
2025-08-08 17:28:23 +0200

    Add pre deployment config changes

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import eslint from 'vite-plugin-eslint';
 
 // https://vite.dev/config/
 export default defineConfig({
+  // base: 'https://cometspaceways.zombiecat.dev/',
   plugins: [react(), eslint()],
 })
