4043e1ed09d39be9bc06bb485886a29e4720761a
David Castejon <ombengue@hotmail.com>
2025-10-25 18:29:28 +0200

    Add error notice - fligh data fetch failure

--- src/components/flight/FlightScheduleLoader.jsx
+++ src/components/flight/FlightScheduleLoader.jsx
@@ -1,26 +1,44 @@
-import { useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import { useNavigate, useLoaderData, useNavigation } from 'react-router';
+import { LayoutContext } from '../../contexts/LayoutContext';
 import useLayout from '../../hooks/useLayout';
+
 import Tickets from '../../pages/purchase/Tickets';
+import ErrorNotice from '../infopieces/ErrorNotice';
 
 import supabase from '../../../db/supabase-client';
 
+import errors from '../infopieces/errorTypes';
+
 function FlightScheduleLoader() {
+    const [dataError, setDataError] = useState(null);
+    const { handlePopupLaunch } = useContext(LayoutContext);
     const flightSchedule = useLoaderData();
     const navigate = useNavigate();
-    const navigation = useNavigation();
-    const { dispatch } = useLayout();
-
-    console.log(navigation.state)
+    /* const navigation = useNavigation();
+    const { dispatch } = useLayout(); */
 
     useEffect(() => {
         navigate('/purchase/tickets/flight-data');
-    }, [])
+    }, []);
+
+    useEffect(() => {
+        if (flightSchedule) {
+            if ("message" in flightSchedule && flightSchedule.message.includes("Error")) {
+                handlePopupLaunch({ modalClass: "generic", content: <ErrorNotice error={errors.flightData} /> })
+                setDataError(true);
+            }
+        }
+    }, []);
 
     /* useEffect(() => {
         console.log("puta")
         dispatch({ type: 'toggle/scroll' })
     }, [navigation.state]) */
+
+    if (dataError) {
+        return <Tickets />
+    }
     
     return <Tickets flightSchedule={flightSchedule} />
 }
@@ -32,10 +50,7 @@ export async function fetchFlights() {
         .from('Origins')
         .select()
 
-        if (error) {
-            console.log(error);
-            return;
-        }
+        if (error) return error;
 
         return data;
     }
