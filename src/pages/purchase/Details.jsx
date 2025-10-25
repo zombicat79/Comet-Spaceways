7e07c99ebbf3517908f8beb17632939d632061c2
David Castejon <ombengue@hotmail.com>
2025-10-25 19:23:02 +0200

    Add work in progress popup to passenger detail page

--- src/pages/purchase/Details.jsx
+++ src/pages/purchase/Details.jsx
@@ -1,4 +1,15 @@
+import { useEffect, useContext } from 'react';
+import { LayoutContext } from '../../contexts/LayoutContext';
+
+import WorkInProgress from '../../components/infopieces/WorkInProgress';
+
 function PassengerDetails() {
+    const { handlePopupLaunch } = useContext(LayoutContext);
+
+    useEffect(() => {
+        handlePopupLaunch({ modalClass: "generic", content: <WorkInProgress /> });
+    }, []);
+
     return (
         <div className="details"></div>
     )
