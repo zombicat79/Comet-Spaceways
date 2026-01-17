f3ce3e5597babe71a18224796664977149f94fd9
David Castejon <ombengue@hotmail.com>
2026-01-17 20:06:18 +0100

    Style destinations page and destination cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,14 +4,14 @@ import { LayoutContext } from './../contexts/LayoutContext';
 
 import WorkInProgress from './infopieces/WorkInProgress';
 
-function Card({ children, bgImg, link }) {
+function Card({ children, height=40, bgImg, link }) {
     const { handlePopupLaunch } = useContext(LayoutContext);
     console.log(link)
 
     if (link) {
         return (
             <Link to={`/destinations/${link}`}>
-                <div className="card element--clickable">
+                <div className={`card card--h${height} element--clickable`}>
                     <div className={`card__img card__img--${bgImg}`}></div>
                     <div className="card__content">
                         {children}
--- src/pages/destinations/DestinationsIndex.jsx
+++ src/pages/destinations/DestinationsIndex.jsx
@@ -2,6 +2,7 @@ import { useContext } from 'react';
 import { DestinationsContext } from '../../contexts/DestinationsContext';
 
 import Card from './../../components/Card';
+import PriceTag from '../../components/PriceTag';
 
 function DestinationsIndex() {
     const { destinations } = useContext(DestinationsContext);
@@ -9,16 +10,25 @@ function DestinationsIndex() {
     if (destinations.length > 0) {
         return (
             <main className="destinations">
-                <h3 className="homepage__offers-heading">OUR DESTINATIONS</h3>
+                <div className="destinations__heading">
+                    <h2>OUR DESTINATIONS WITHIN THE SOLAR SYSTEM</h2>
+                    <p></p>
+                </div>
                 <div className="destinations__list">
                     {destinations.map((el) => {
                         return (
                             <Card key={el.id} bgImg={el.cover_img} link={el.port}>
                                 <div className="card__heading">
                                     <h4 className="card__title">{el.full_name}</h4>
+                                    <h5 className="card__subtitle">{el.alias} - ({el.port})</h5>
                                 </div>
                                 <div className="card__body">
-                                    
+                                    <p>{el.intro}</p>
+                                    <PriceTag 
+                                        price="45" 
+                                        priceType="initial"
+                                        currency="au"
+                                    />
                                 </div>
                             </Card>
                         )
