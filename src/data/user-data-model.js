3ff103a66a61f1f3a72c080faa58e9ae4287e582
David Castejon <ombengue@hotmail.com>
2026-03-26 16:40:23 +0100

    Complete Sign Up page and User data model

--- src/data/user-data-model.js
+++ src/data/user-data-model.js
@@ -29,31 +29,31 @@ const userProgressData = {
 
 const raceVitalsAdjustment = {
     green_little_man: {
-        health: -5, strength: -4, intelligence: 3, wisdom: 0, dexterity: 2, diplomacy: -1, skills: ["stealthiness", "abduction", "emotion-suppression"]
+        health: -5, strength: -4, intelligence: 3, wisdom: 0, dexterity: 2, diplomacy: -1, skills: ["stealthiness", "cooperation", "emotion-suppression"]
     },
     reptilian: {
-        health: 6, strength: 8, intelligence: 0, wisdom: 2, dexterity: -2, diplomacy: -3, skills: ["subjugation", "ambushing"]
+        health: 6, strength: 8, intelligence: 0, wisdom: 2, dexterity: -2, diplomacy: -3, skills: ["subjugation", "warfare", "resource-extraction"]
     },
     insectoid: {
-        health: 4, strength: 5, intelligence: 1, wisdom: 3, dexterity: 6, diplomacy: 0, skills: [""]
+        health: 4, strength: 5, intelligence: 1, wisdom: 3, dexterity: 6, diplomacy: 0, skills: ["cooperation", "multitasking", "emotion-suppression"]
     },
     cephalopodian: {
-        health: 0, strength: 0, intelligence: 5, wisdom: 5, dexterity: 7, diplomacy: 3, skills: []
+        health: 0, strength: 0, intelligence: 5, wisdom: 5, dexterity: 7, diplomacy: 3, skills: ["multitasking", "stealthiness", "navigation"]
     },
     polymorphic: {
-        health: 3, strength: 2, intelligence: -2, wisdom: -2, dexterity: 5, diplomacy: -5, skills: ["impersonation"]
+        health: 3, strength: 2, intelligence: -2, wisdom: -2, dexterity: 5, diplomacy: -5, skills: ["transmutation", "regeneration", "emotion-suppression"]
     },
     big_headed_gray: {
-        health: -2, strength: -4, intelligence: 6, wisdom: 5, dexterity: 2, diplomacy: 2, skills: ["telepathy"]
+        health: -2, strength: -4, intelligence: 6, wisdom: 5, dexterity: 2, diplomacy: 2, skills: ["psionics", "subjugation", "superthinking"]
     },
     amoebian: {
-        health: 0, strength: 0, intelligence: -5, wisdom: -5, dexterity: 0, diplomacy: -4, skills: [""]
+        health: 0, strength: 0, intelligence: -5, wisdom: -5, dexterity: 0, diplomacy: -4, skills: ["transversality", "regeneration", "stealthiness"]
     },
     leonian: {
-        health: 5, strength: 7, intelligence: 0, wisdom: 2, dexterity: 2, diplomacy: 0, skills: [""]
+        health: 5, strength: 7, intelligence: 0, wisdom: 2, dexterity: 2, diplomacy: 0, skills: ["warfare", "navigation", "resource-extraction"]
     },
     aetherian: {
-        health: -2, strength: -2, intelligence: 7, wisdom: 8, dexterity: 0, diplomacy: 7, skills: ["transcendence"]
+        health: -2, strength: -2, intelligence: 7, wisdom: 8, dexterity: 0, diplomacy: 7, skills: ["transversality", "psionics", "self-sustenance"]
     },
 }
 
@@ -61,37 +61,37 @@ const raceVitalsAdjustment = {
 
 const nationalityVitalsAdjustment = {
     selenyte: {
-        health: 0, strength: -1, intelligence: 0, wisdom: 0, dexterity: 1, diplomacy: 0, skills: []
+        health: 0, strength: -1, intelligence: 0, wisdom: 0, dexterity: 1, diplomacy: 0, skills: ["multitasking"]
     },
     earthling: {
-        health: 1, strength: 0, intelligence: 0, wisdom: 0, dexterity: 0, diplomacy: 1, skills: []
+        health: 1, strength: 0, intelligence: 0, wisdom: 0, dexterity: 0, diplomacy: 1, skills: ["subjugation"]
     },
     martian: {
-        health: 0, strength: 0, intelligence: 0, wisdom: 0, dexterity: 0, diplomacy: 1, skills: []
+        health: 0, strength: 0, intelligence: 0, wisdom: 0, dexterity: 0, diplomacy: 1, skills: ["multitasking"]
     },
     venusian: {
-        health: 0, strength: 0, intelligence: 0, wisdom: 0, dexterity: 0, diplomacy: 0, skills: []
+        health: 0, strength: 0, intelligence: 0, wisdom: 0, dexterity: 0, diplomacy: 0, skills: ["navigation"]
     },
     belter: {
-        health: -1, strength: -2, intelligence: 0, wisdom: 0, dexterity: 2, diplomacy: 1, skills: []
+        health: -1, strength: -2, intelligence: 0, wisdom: 0, dexterity: 2, diplomacy: 1, skills: ["resource-extraction"]
     },
     jovian: {
-        health: -1, strength: 1, intelligence: 0, wisdom: 1, dexterity: 1, diplomacy: 0, skills: []
+        health: -1, strength: 1, intelligence: 0, wisdom: 1, dexterity: 1, diplomacy: 0, skills: ["self-sustenance"]
     },
     saturnian: {
-        health: -1, strength: 1, intelligence: 0, wisdom: 1, dexterity: 1, diplomacy: 0, skills: []
+        health: -1, strength: 1, intelligence: 0, wisdom: 1, dexterity: 1, diplomacy: 0, skills: ["self-sustenance"]
     }
 }
 
 const originVitalsAdjustment = {
     milky_way: {
-        health: 0, strength: 0, intelligence: 0, wisdom: 0, dexterity: 0, diplomacy: 0, skills: ["navigation"]
+        health: 0, strength: 0, intelligence: 0, wisdom: 0, dexterity: 0, diplomacy: 0, skills: ["communication"]
     },
     other_galaxies: {
-        health: 0, strength: 0, intelligence: 0, wisdom: 1, dexterity: 0, diplomacy: 1, skills: [""]
+        health: 0, strength: 0, intelligence: 0, wisdom: 1, dexterity: 0, diplomacy: 1, skills: ["navigation"]
     },
     other_dimensions: {
-        health: 0, strength: 0, intelligence: 0, wisdom: 2, dexterity: 0, diplomacy: -1, skills: []
+        health: 0, strength: 0, intelligence: 0, wisdom: 2, dexterity: 0, diplomacy: -1, skills: ["transversality"]
     }
 }
 
@@ -99,13 +99,13 @@ const originVitalsAdjustment = {
 
 const buildVitalsAdjustment = {
     organic: {
-        health: 2, strength: 2, intelligence: 3, wisdom: 2, dexterity: 2, diplomacy: 4, skills: []
+        health: 2, strength: 2, intelligence: 3, wisdom: 2, dexterity: 2, diplomacy: 4, skills: ["communication", "resource-extraction", "cooperation"]
     },
     cyborg: {
-        health: 2, strength: 5, intelligence: 6, wisdom: 4, dexterity: 3, diplomacy: -3, skills: ["self-repair"]
+        health: 2, strength: 5, intelligence: 6, wisdom: 4, dexterity: 3, diplomacy: -3, skills: ["regeneration", "psionics", "superthinking"]
     },
     android: {
-        health: 4, strength: 6, intelligence: 6, wisdom: 4, dexterity: 0, diplomacy: -1, skills: ["self-repair"]
+        health: 4, strength: 6, intelligence: 6, wisdom: 4, dexterity: 0, diplomacy: -1, skills: ["self-sustenance", "multitasking", "superthinking"]
     }
 }
 
@@ -113,10 +113,10 @@ const buildVitalsAdjustment = {
 
 const genderVitalsAdjustment = {
     male: {
-        health: 0, strength: 1, intelligence: 0, wisdom: 0, dexterity: 1, diplomacy: 0, skills: []
+        health: 0, strength: 1, intelligence: 0, wisdom: 0, dexterity: 1, diplomacy: 0, skills: ["navigation"]
     },
     female: {
-        health: 0, strength: 0, intelligence: 1, wisdom: 0, dexterity: 0, diplomacy: 1, skills: []
+        health: 0, strength: 0, intelligence: 1, wisdom: 0, dexterity: 0, diplomacy: 1, skills: ["communication"]
     },
     other: {
         health: 0, strength: 0, intelligence: 0, wisdom: 0, dexterity: 0, diplomacy: 0, skills: []
@@ -127,52 +127,52 @@ const genderVitalsAdjustment = {
 
 const jobVitalsAdjustment = {
     astrophysicist: {
-        health: 0, strength: 0, intelligence: 3, wisdom: 5, dexterity: 0, diplomacy: 0, skills: ["navigation"]
+        health: 0, strength: 0, intelligence: 3, wisdom: 5, dexterity: 0, diplomacy: 0, skills: ["navigation", "superthinking", "communication"]
     },
     ai_psychologist: {
-        health: 0, strength: 0, intelligence: 3, wisdom: 3, dexterity: 0, diplomacy: 3, skills: []
+        health: 0, strength: 0, intelligence: 3, wisdom: 3, dexterity: 0, diplomacy: 3, skills: ["subjugation", "emotion-suppression"]
     },
     pirate: {
-        health: 2, strength: 3, intelligence: 0, wisdom: 0, dexterity: 2, diplomacy: -2, skills: []
+        health: 2, strength: 3, intelligence: 0, wisdom: 0, dexterity: 2, diplomacy: -2, skills: ["stealthiness", "warfare", "resource-extraction"]
     },
     colonist: {
-        health: 1, strength: 0, intelligence: 0, wisdom: 1, dexterity: 3, diplomacy: 2, skills: []
+        health: 1, strength: 0, intelligence: 0, wisdom: 1, dexterity: 3, diplomacy: 2, skills: ["self-sustenance", "multitasking"]
     },
     habitat_builder: {
-        health: 0, strength: 1, intelligence: 1, wisdom: 1, dexterity: 4, diplomacy: 0, skills: []
+        health: 0, strength: 1, intelligence: 1, wisdom: 1, dexterity: 4, diplomacy: 0, skills: ["multitasking", "transmutation"]
     },
     cybersurgeon: {
-        health: 1, strength: 0, intelligence: 1, wisdom: 2, dexterity: 4, diplomacy: 1, skills: [""]
+        health: 1, strength: 0, intelligence: 1, wisdom: 2, dexterity: 4, diplomacy: 1, skills: ["regeneration"]
     },
     vr_soldier: {
-        health: 2, strength: 1, intelligence: 1, wisdom: 0, dexterity: 2, diplomacy: 0, skills: []
+        health: 2, strength: 1, intelligence: 1, wisdom: 0, dexterity: 2, diplomacy: 0, skills: ["warfare", "multitasking"]
     },
     asteroid_miner: {
-        health: -2, strength: 3, intelligence: 0, wisdom: 0, dexterity: 3, diplomacy: 0, skills: []
+        health: -2, strength: 3, intelligence: 0, wisdom: 0, dexterity: 3, diplomacy: 0, skills: ["resource-extraction", "cooperation"]
     },
     invasion: {
-        health: 2, strength: 3, intelligence: 1, wisdom: 0, dexterity: 0, diplomacy: -2, skills: ["subjugation"]
+        health: 2, strength: 3, intelligence: 1, wisdom: 0, dexterity: 0, diplomacy: -2, skills: ["subjugation", "warfare", "resource-extraction"]
     },
     colonization: {
-        health: 1, strength: 0, intelligence: 1, wisdom: 1, dexterity: 3, diplomacy: 2, skills: ["harvesting", ""]
+        health: 1, strength: 0, intelligence: 1, wisdom: 1, dexterity: 3, diplomacy: 2, skills: ["self-sustenance", "resource-extraction"]
     },
     exploration: {
-        health: 0, strength: 0, intelligence: 2, wisdom: 2, dexterity: 1, diplomacy: 3, skills: ["navigation"]
+        health: 0, strength: 0, intelligence: 2, wisdom: 2, dexterity: 1, diplomacy: 3, skills: ["navigation", "communication"]
     },
     invention: {
-        health: 0, strength: 0, intelligence: 4, wisdom: 3, dexterity: 4, diplomacy: 0, skills: [""]
+        health: 0, strength: 0, intelligence: 4, wisdom: 3, dexterity: 4, diplomacy: 0, skills: ["multitasking", "superthinking", "resource-extraction"]
     },
     scientific_research: {
-        health: 0, strength: 0, intelligence: 2, wisdom: 4, dexterity: 1, diplomacy: 0, skills: [""]
+        health: 0, strength: 0, intelligence: 2, wisdom: 4, dexterity: 1, diplomacy: 0, skills: ["superthinking", "transmutation", "transversality"]
     },
     philosophy: {
-        health: 0, strength: 0, intelligence: 2, wisdom: 5, dexterity: 0, diplomacy: 2, skills: ["transcendence"]
+        health: 0, strength: 0, intelligence: 2, wisdom: 5, dexterity: 0, diplomacy: 2, skills: ["superthinking", "cooperation", "emotion-suppression"]
     },
     religion: {
-        health: 0, strength: 0, intelligence: 1, wisdom: 4, dexterity: 0, diplomacy: 4, skills: ["transcendence", "subjugation"]
+        health: 0, strength: 0, intelligence: 1, wisdom: 4, dexterity: 0, diplomacy: 4, skills: ["subjugation", "communication"]
     },
     language_interpretation: {
-        health: 0, strength: 0, intelligence: 2, wisdom: 2, dexterity: 1, diplomacy: 3, skills: ["translation", "communication"]
+        health: 0, strength: 0, intelligence: 2, wisdom: 2, dexterity: 1, diplomacy: 3, skills: ["communication", "cooperation"]
     }
 }
 
