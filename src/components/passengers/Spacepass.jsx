8dc0ed92c176d889603e304c7f5da673a55901c7
David Castejon <ombengue@hotmail.com>
2026-04-23 17:10:18 +0200

    Complete Spacepass component and display specimen on NHEs page

--- src/components/passengers/Spacepass.jsx
+++ src/components/passengers/Spacepass.jsx
@@ -1,17 +1,75 @@
+import { format, add } from "date-fns";
+
 function Spacepass({ userData }) {
     const defaultPicString = '/assets/images/characters/profiles/profile-default.png';
 
     return (
-        <figure className='spacepass'>
+        <figure className={userData ? "spacepass" : "spacepass spacepass--generic"}>
             <div className="spacepass__header">
-                <p>solar federation</p>
-                <p>spacepass</p>
+                <p className="spacepass__provider">solar federation</p>
+                <p className="spacepass__title">spacepass</p>
             </div>
             <div className="spacepass__body">
-                <img className='spacepass__picture' src={userData ? '' : defaultPicString} alt="spacepass holder picture" />
-                <div className='spacepass__data'></div>
+                <img className="spacepass__picture" src={userData ? '' : defaultPicString} alt="spacepass holder picture" />
+                <div className="spacepass__data">
+                    <div className="data__row">
+                        <div className="data__item">
+                            <p className="data__label">Type</p>
+                            <p className="data__value">B1</p>
+                        </div>
+                        <div className="data__item">
+                            <p className="data__label">Authority</p>
+                            <p className="data__value">SF / EEU</p>
+                        </div>
+                        <div className="data__item">
+                            <p className="data__label">Spacepass No.</p>
+                            <p className="data__value">AAA000000</p>
+                        </div>
+                    </div>
+                    <div className="data__row">
+                        <div className="data__item">
+                            <p className="data__label">Tribe / Cast / Faction</p>
+                            <p className="data__value">SAMPLE FACTION</p>
+                        </div>
+                    </div>
+                    <div className="data__row">
+                        <div className="data__item">
+                            <p className="data__label">Name</p>
+                            <p className="data__value">SAMPLE NAME</p>
+                        </div>
+                    </div>
+                    <div className="data__row">
+                        <div className="data__item">
+                            <p className="data__label">Race</p>
+                            <p className="data__value">SAMPLE RACE</p>
+                        </div>
+                    </div>
+                    <div className="data__row">
+                        <div className="data__item">
+                            <p className="data__label">Date of issue</p>
+                            <p className="data__value">
+                                {format(add(new Date(), { years: 100 }).toDateString(), 'dd-MM-yyyy')}
+                            </p>
+                        </div>
+                        <div className="data__item">
+                            <p className="data__label">Date of expiry</p>
+                            <p className="data__value">
+                                {format(add(new Date(), { years: 105 }).toDateString(), 'dd-MM-yyyy')}
+                            </p>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className="spacepass__footer"></div>
+            <div className="spacepass__footer">
+                <p className="spacepass__encoding">
+                    {'P<SFSAMPLEFACTION<SAMPLENAME<<<<<<<<<<<<<<<<<<<<<<<<<'}
+                </p>
+                <p className="spacepass__encoding">
+                    {'AAA000000SF743499958HJ744424<<<<<<<<<<<<<<<<<<<<<EEU<<<<'}
+                </p>
+            </div>
+
+            {!userData && <p className="specimen-tag">*-- SPECIMEN --*</p>}
         </figure>
     )
 }
--- src/pages/info/nhes/NheIndex.jsx
+++ src/pages/info/nhes/NheIndex.jsx
@@ -1,10 +1,11 @@
 import { useState, useEffect, useCallback } from 'react';
-import { useLoaderData, useNavigate, useLocation } from 'react-router';
+import { useLoaderData, useNavigate, useLocation, Link } from 'react-router';
 
 import ContentSection from './../../../layout/ContentSection';
 import SliderTool from '../../../components/SliderTool';
 import PromoPoster from '../../../components/PromoPoster';
 import Spacepass from './../../../components/passengers/Spacepass';
+import Button from '../../../components/Button';
 
 import supabase from '../../../../db/supabase-client';
 import { writeToStorage, readFromStorage } from '../../../utilities/utils';
@@ -51,13 +52,19 @@ function NheIndex() {
         <main className="nhes">
             <section className="nhes__intro">
                 <h1 className="nhes__heading">TRAVELING AS A NON-HUMAN ENTITY (Nhe)</h1>
-                <p className="nhes__paragraph">
-                    Since the landmark passage of the Alien Liberty of Movement Act in 2094 all alien passengers are allowed to travel 
-                    within the boundaries of the Solar Systemo. This historic legislation, approved by the Senate of the Solar Federation, officially mandates that non-human travelers be permitted to board commercial flights and travel normally between all established destinations within the Solar System.
+                <p className="nhes__paragraph mb-2">
+                    Since the landmark passage of the <span>Alien Liberty of Movement Act</span> Act in 2094 all alien passengers 
+                    are allowed to travel between any two given points within the boundaries of the Solar System. This historic legislation, 
+                    approved by the Senate of the Solar Federation and sanctioned by its Supreme AI Counsellor, officially mandates that 
+                    non-human travelers be permitted to board commercial flights and travel normally between all worlds, colonies and civilian 
+                    outposts under the rule of the united humanoid civilization.
                 </p>
                 <p className="nhes__paragraph">
-                    In compliance on board Comet Spaceways flights
+                    In compliance with current law provisions, we at <span>Comet Spaceways</span> guarantee all of our non-human customers 
+                    their right to access our transport services, provided they prove themselves eligible by race and capable of meeting the 
+                    legal requirements listed further below by the time of their scheduled departure: 
                 </p>
+                <img src="/logos/sfederation-logo_light.png" style={{width: "200px"}} />
             </section>
             <section className="nhes__races">
                 <h2 className="nhes__subheading">PERMITTED RACES</h2>
@@ -85,18 +92,39 @@ function NheIndex() {
             <section className="nhes__requirements">
                 <h2 className="nhes__subheading">REQUIREMENTS</h2>
                 <ContentSection>
-                    <h3>1. Spacepass</h3>
-                    <p className="nhes__paragraph">
-                        In accordance with current law provisions established by the of the Government of the Solar Federation all passengers 
-                        (human or non-human) must be in possession of a valid Spacepass in order to travel within the boundaries of the 
-                        Solar System
+                    <h3 className="content-section__heading">1. Spacepass</h3>
+                    <p className="content-section__paragraph">
+                        In accordance with current law provisions and directives established by the Government of the Solar Federation, all 
+                        passengers (human or non-human) must be in possession of a valid Spacepass in order to travel within the boundaries 
+                        of the Solar System:
                     </p>
                     <Spacepass />
-                    <p>Spacepasses must be valid at the time of boarding a flight</p>
+                    <p className="content-section__paragraph text-bold text-alert">
+                        <span style={{fontSize: '30px'}}>⚠️</span> All spacepasses must be valid at scheduled boarding time!
+                    </p>
+                    <ul className="content-section__list content-section__list--numeric">
+                        <p className="list-title">HOW TO</p>
+                        <li className="list-element">
+                            <p className="content-section__paragraph">
+                                New spacepasses will be obtained upon the creation of a new user account with Comet Spaceways:
+                            </p>
+                            <Link to="/create-account">
+                                <Button type="secondary" text="Create account" />
+                            </Link>
+                        </li>
+                        <li className="list-element">
+                            <p className="content-section__paragraph">
+                                Issues with expired or invalid spacepasses may be solved at the relevant consulates of the Solar Federation:
+                            </p>
+                            <Link to="/destinations">
+                                <Button type="secondary" text="Check stellar map" />
+                            </Link>
+                        </li>
+                    </ul>
                 </ContentSection>
                 <ContentSection>
-                    <h3>2. Goodwill Declaration</h3>
-                    <p className="nhes__paragraph">
+                    <h3 className="content-section__heading">2. Declaration of Goodwill</h3>
+                    <p className="content-section__paragraph">
                         The Alien Liberty of Movement Act establishes that all alien passengers willing to travel within the 
                         boundaries of the Solar System must complete a declaration of godwill towards humanity, thereby explicitly refraining of 
                         participating in any activities whatsoever that may lead to or result in harm to human civilization and/or any of its 
@@ -105,8 +133,8 @@ function NheIndex() {
                     <p>In compliance with Comet Spaceways demands its alien passengers to </p>
                 </ContentSection>
                 <ContentSection>
-                    <h3>3. Effective Communication Capacity</h3>
-                    <p className="nhes__paragraph">
+                    <h3 className="content-section__heading">3. Effective Communication Capacity</h3>
+                    <p className="content-section__paragraph">
                         It is also established in the Alien Liberty of Movement Act establishes that all alien passengers willing to 
                         travel within the boundaries of the Solar System must be able to effectively communicate with flight crew members 
                         at all time
